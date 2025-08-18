676107b68acd3a70a1c5bb352ea16c7afc1feeb8
45Hnri <mailhenrimal@web.de>
2025-08-18 14:53:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">unshort</h1>
+
+> [!NOTE]
+> Hides Youtube shorts on your homepage to stop you from wasting your time.
---
+++ unshort.js
@@ -0,0 +1,88 @@
+main();
+
+// 1. Check until PageManager exists
+function main() {
+    observe(
+        document.body,
+        act_init,
+        { subtree: true, childList: true },
+        getPageManager,
+    );
+}
+
+// 2. Disconnect body observer, check PageManager
+function act_init(_, obs) {
+    obs.disconnect();
+    observe(getPageManager(), act_pageManager);
+}
+
+// 3. Initialize shortCatcher or Check primary content browser
+function act_pageManager(mut) {
+    removeShortNav();
+    mut.forEach((u) =>
+        u.addedNodes.forEach((n) => {
+            if (n.nodeName === "YTD-BROWSE" && n.ELEMENT_NODE) {
+                const contents = n.querySelector("[id=contents]");
+                if (contents) {
+                    observe(contents, act_shortCatcher);
+                } else {
+                    observe(
+                        n.querySelector("div[id=primary]"),
+                        act_findSubContents,
+                    );
+                }
+            }
+        }),
+    );
+}
+
+// 4. Check for contents Element
+function act_findSubContents(mut, obs) {
+    mut.forEach((u) => {
+        u.addedNodes.forEach((n) => {
+            if (n.nodeName === "YTD-RICH-GRID-RENDERER" && n.ELEMENT_NODE) {
+                const contents = n.querySelector("div[id=contents]");
+                obs.disconnect();
+                observe(contents, act_shortCatcher);
+            }
+        });
+    });
+}
+
+// 5. Find shorts from Updates and remove
+function act_shortCatcher(mut) {
+    mut.forEach((m) =>
+        m.addedNodes.forEach((n) => {
+            if (
+                n.nodeName === "YTD-RICH-SECTION-RENDERER" &&
+                n.ELEMENT_NODE &&
+                n.querySelector("ytd-rich-shelf-renderer")?.attributes[
+                    "is-shorts"
+                ]
+            )
+                n.remove();
+        }),
+    );
+}
+
+function removeShortNav() {
+    document
+        .querySelector("ytd-mini-guide-entry-renderer[aria-label=Shorts]")
+        ?.remove();
+}
+
+function getPageManager() {
+    return document.querySelector("ytd-page-manager");
+}
+
+function observe(
+    node,
+    action,
+    config = { childList: true },
+    condition = () => true,
+) {
+    const observer = new MutationObserver((mutations) => {
+        if (condition()) action(mutations, observer);
+    });
+    observer.observe(node, config);
+}
